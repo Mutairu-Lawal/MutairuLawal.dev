adfd6cb695d27fb8a725ba0cd97d61113fd19858
Mutairu <thebillionnairadream@gmail.com>
2024-12-26 12:07:57 +0000

    `Added AboutMe component and updated styles`

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { useContext } from 'react';
 import Header from './components/Header';
 import { ThemeContext } from './contextApi/themeContext';
 import Hero from './components/Hero';
+import AboutMe from './components/AboutMe';
 
 function App() {
   const { darkMode } = useContext(ThemeContext);
@@ -12,6 +13,7 @@ function App() {
       <div className={`${darkMode && 'dark'}`}>
         <Header />
         <Hero />
+        <AboutMe />
       </div>
     </>
   );
---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,51 @@
+import myPhoto2 from '../assets/images/main-pic.jpg';
+
+export default function AboutMe() {
+  return (
+    <section className="bg-gray-50 dark:bg-gray-900 dark:text-gray-100">
+      <h1 className="bg-gray-200 px-5 py-1 w-fit mx-auto rounded-xl text-gray-600 text-[14px] font-medium leading-5 mb-20 dark:bg-gray-800 dark:text-gray-300">
+        About me
+      </h1>
+      <div className="md:grid md:grid-cols-2 md:justify-between md:items-center md:gap-10 lg:gap-20">
+        <div className="hero-image flex place-content-center mx-auto lg:mx-0 md:place-content-start">
+          <div className="bg-gray-200 dark:bg-gray-800 px-4 sm:px-8">
+            <div className="w-[280px] h-[320px] lg:w-[400px] lg:h-[480px]">
+              <img
+                src={myPhoto2}
+                alt="my photo"
+                className="w-full h-full object-cover relative bottom-10 border-gray-50 dark:border-gray-900 border-8"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="mt-10 space-y-4 py-3 md:mt-0">
+          <h1 className="text-2xl font-semibold md:text-3xl">
+            Curious about me? Here you go!
+          </h1>
+          <div className="text-base space-y-4">
+            <p>
+              Hi, I’m Mutairu Lawal, a front-end developer from Nigeria with a
+              passion for crafting beautiful and functional web experiences.
+            </p>
+            <p>
+              With expertise in web development technologies and modern
+              frameworks on building responsive and user-friendly designs that
+              meet modern web standards. I’m dedicated to staying current with
+              the latest trends in front-end development and constantly honing
+              my skills to deliver innovative solutions.
+            </p>
+            <p>
+              My mission is to create impactful web applications that solve
+              real-world challenges and inspire positive change. I thrive in
+              collaborative environments and am always excited to connect,
+              learn, and grow with like-minded professionals.
+            </p>
+            <p>
+              Let’s build something amazing together! I promise i don't bite 😉
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}

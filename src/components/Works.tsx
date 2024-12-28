c3538915bb80cb1f8fe559bd13babcacf0739320
Mutairu-Lawal <YourEmail@example.com>
2024-12-28 23:14:26 +0000

    `Refactor Works component to use data from projects array`

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,8 +1,16 @@
 import SectionHeading from './SectionHeading';
 import { BiLinkExternal } from 'react-icons/bi';
-import project from '../assets/project-img/moniepoint.jpg';
+import { projects as works } from '../data/projects';
 
 export default function Works() {
+  const btns = (arryOfLang: string[]) => {
+    return arryOfLang.map((lang) => (
+      <p className="text-sm font-medium px-5 py-1 bg-gray-200 text-gray-600 max-w-max rounded-full dark:bg-gray-400 dark:text-gray-200">
+        {lang}
+      </p>
+    ));
+  };
+
   return (
     <section id="work">
       <SectionHeading
@@ -12,53 +20,48 @@ export default function Works() {
       <div
         data-aos="fade-up"
         data-aos-duration="1000"
-        className="projects mt-10"
+        className="projects mt-10 space-y-5"
       >
-        <div className="card shadow-md rounded-xl  dark:bg-gray-800 overflow-hidden grid md:grid-cols-2 ">
-          <div className="project-img p-8 lg:p-10 bg-gray-100 flex flex-1 justify-center items-center dark:bg-gray-900">
-            <div className="w-ful h-[300px] overflow-hidden rounded-xl hover:scale-105 duration-1000 ease-in-out cursor-pointer">
-              <img
-                src={project}
-                alt=""
-                className="w-full h-full object-cover"
-              />
+        {works.map((work) => (
+          <div className="card shadow-md rounded-xl  dark:bg-gray-800 overflow-hidden grid md:grid-cols-2 ">
+            <div className="project-img p-8 lg:p-10 bg-gray-100 flex flex-1 justify-center items-center dark:bg-gray-900">
+              <div className="w-ful h-[300px] overflow-hidden rounded-xl hover:scale-105 duration-1000 ease-in-out cursor-pointer">
+                <a href={work.link} target="_blank">
+                  <img
+                    src={work.imageUrl}
+                    alt={`${work.title} project image`}
+                    className="w-full h-full object-cover"
+                  />
+                </a>
+              </div>
             </div>
-          </div>
-          <div className="project-description flex place-content-center">
-            <div className="p-8 space-y-4 md:flex md:flex-col md:justify-center">
-              <h1 className="font-semibold text-lg text-gray-900 dark:text-gray-100">
-                Facebook
-              </h1>
-              <p className="text-base font-normal text-gray-600 dark:text-gray-300">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Repellendus cumque est deleniti esse. Amet ipsa, impedit
-                officiis nihil pariatur nam quos tempore, atque soluta, sapiente
-                cumque qui voluptatum optio perferendis.
-              </p>
+            <div className="project-description flex place-content-center">
+              <div className="p-8 space-y-4 md:flex md:flex-col md:justify-center">
+                <h1 className="font-semibold text-lg text-gray-900 dark:text-gray-100">
+                  {work.title}
+                </h1>
+                <p className="text-base font-normal text-gray-600 dark:text-gray-300">
+                  {work.description}
+                </p>
 
-              <div className="framework flex gap-2 flex-wrap">
-                <p className="text-sm font-medium px-5 py-1 bg-gray-200 text-gray-600 max-w-max rounded-full dark:bg-gray-400 dark:text-gray-200">
-                  React
-                </p>
-                <p className="text-sm font-medium px-5 py-1 bg-gray-200 text-gray-600 max-w-max rounded-full dark:bg-gray-400 dark:text-gray-200">
-                  javascript
-                </p>
-                <p className="text-sm font-medium px-5 py-1 bg-gray-200 text-gray-600 max-w-max rounded-full dark:bg-gray-400 dark:text-gray-200">
-                  tailwindcss
-                </p>
-                <p className="text-sm font-medium px-5 py-1 bg-gray-200 text-gray-600 max-w-max rounded-full dark:bg-gray-400 dark:text-gray-200">
-                  bootsstrap
-                </p>
-                <p className="text-sm font-medium px-5 py-1 bg-gray-200 text-gray-600 max-w-max rounded-full dark:bg-gray-400 dark:text-gray-200">
-                  nextjs
-                </p>
-              </div>
-              <div className="hover:cursor-pointer text-2xl hover:bg-gray-100 max-w-max rounded-lg dark:hover:bg-gray-900 dark:text-gray-100 p-2">
-                <BiLinkExternal />
+                <div className="framework flex gap-2 flex-wrap">
+                  {btns(work.languages)}
+                </div>
+
+                <a
+                  href={work.link}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="inline-flex"
+                >
+                  <div className="hover:cursor-pointer text-2xl hover:bg-gray-100 max-w-max rounded-lg dark:hover:bg-gray-900 dark:text-gray-100 p-2 mt-3s">
+                    <BiLinkExternal />
+                  </div>
+                </a>
               </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </section>
   );
---
+++ src/data/projects.ts
@@ -0,0 +1,75 @@
+import moniepointLoginPage from '../assets/project-img/moniepoint.jpg';
+import rockPaperScissors from '../assets/project-img/rock-paper-scissors.jpeg';
+import BrainWave from '../assets/project-img/BrainWave_.png';
+import pigGame from '../assets/project-img/pig_game.png';
+import modrino from '../assets/project-img/modrino.png';
+import foodbox from '../assets/project-img/foodbox.png';
+import quizFiesta from '../assets/project-img/quiz-fiesta.png';
+import shopNow from '../assets/project-img/shopNow.png';
+
+export const projects = [
+  {
+    id: 7,
+    title: 'ShopNow',
+    description: `e-commerce website that allows users to browse and purchase products online. The website features a user-friendly interface, allowing users to easily navigate through the website and view products. The website also includes a shopping cart feature, allowing users to add and remove items from their cart. The website also includes a checkout process, allowing users to securely enter their payment information and complete their purchase.`,
+    imageUrl: shopNow,
+    languages: ['JavaScript', 'HTML', 'CSS', 'Bootstrap'],
+    link: `https://mutairu-lawal.github.io/kc_Task7/`,
+  },
+  {
+    id: 7,
+    title: 'Quiz Fiesta',
+    description: `This website that allows users to take quizzes on various topics. The website features a user-friendly interface, allowing users to easily navigate through the quizzes and view their results.`,
+    imageUrl: quizFiesta,
+    languages: ['HTML', 'CSS', 'JavaScript', 'Bootstrap'],
+    link: `https://quiz-fiesta.netlify.app/`,
+  },
+  {
+    id: 6,
+    title: 'FoodBox',
+    description: `food delivery at its best! this website that allows users to browse and purchase food from various restaurants. The website features a user-friendly interface, allowing users to easily search for restaurants, view menus, and make purchases.`,
+    imageUrl: foodbox,
+    languages: ['HTML', 'CSS', 'Bootstrap'],
+    link: `https://foodbox-clone.netlify.app/`,
+  },
+  {
+    id: 5,
+    title: 'Modrino',
+    description: `LIFE IS EASY!!! This website allow users browse and purchase cars. The website features a user-friendly interface, allowing users to easily search for cars, view detailed information about each car, and make purchases.`,
+    imageUrl: modrino,
+    languages: ['HTML', 'CSS', 'Bootstrap', 'Swiper js'],
+    link: `https://modrino-clone.netlify.app/`,
+  },
+  {
+    id: 4,
+    title: 'dice-game',
+    description: `A simple dice game where two players roll a dice and the player with the highest number wins.`,
+    imageUrl: pigGame,
+    languages: ['HTML', 'CSS', 'JavaScript'],
+    link: `https://mutairu-lawal.github.io/kodeCampTask5/`,
+  },
+  {
+    id: 3,
+    title: 'BrainWave Academy',
+    description: `A comprehensive online learning platform designed to provide users with a wide range of courses and resources to enhance their skills and knowledge.`,
+    imageUrl: BrainWave,
+    languages: ['HTML', 'CSS', 'Bootstrap'],
+    link: `https://mutairu-lawal.github.io/BrainWave/`,
+  },
+  {
+    id: 2,
+    title: 'Rock Paper Scissors Game',
+    description: `A simple web-based Rock-Paper-Scissors game where the user competes against the computer. Built with HTML, CSS, and JavaScript, the game features real-time results, score tracking, and a user-friendly interface.`,
+    imageUrl: rockPaperScissors,
+    languages: ['HTML', 'CSS', 'JavaScript'],
+    link: `https://mutairu-lawal.github.io/rock-paper-scissors/`,
+  },
+  {
+    id: 1,
+    title: 'Moniepoint Login Page',
+    description: `A replica of a Moniepont microfinance bank login page, built from scratch to mimic the original's design and functionality. This project demonstrates HTML and CSS. Features: Identical design and layout to the original functional username and password input fields Responsive design for various devices and screen sizes Demonstrates understanding of front-end development best practices and design principles.`,
+    imageUrl: moniepointLoginPage,
+    languages: ['HTML', 'CSS'],
+    link: `https://mutairu-lawal.github.io/moniepoint-login-page-clone/`,
+  },
+];

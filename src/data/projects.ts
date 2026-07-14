c17d7dda0ecd658b0c41195e72ea5eeec7a521f6
Mutairu-Lawal <thebillionnairadream@gmail.com>
2026-07-14 01:18:07 +0100

    Add learnFlow project with details and image to projects list

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,15 +1,29 @@
-// import moniepointLoginPage from '../assets/project-img/moniepoint.webp';
-// import rockPaperScissors from '../assets/project-img/rock-paper-scissors.webp';
-// import BrainWave from '../assets/project-img/BrainWave_.webp';
-// import pigGame from '../assets/project-img/pig_game.webp';
-// import modrino from '../assets/project-img/modrino.webp';
-// import foodbox from '../assets/project-img/foodbox.webp';
-// import quizFiesta from '../assets/project-img/quiz-fiesta.webp';
 import shopNow from '../assets/project-img/shopNow.webp';
 import quizWhizz from '../assets/project-img/quizzwhizz.webp';
 import myPiggyVestClone from '../assets/project-img/piggyVest-desktop-loginPage.png';
+import learnFlow from '../assets/project-img/learnflow-dashboard.webp';
 
 export const projects = [
+  {
+    id: 71420260109,
+    title: 'learnFlow',
+    description:
+      'A web-based platform lms  designed to help users learn and test their knowledge through structured content and interactive quizzes. It combines educational resources with assessment tools to create a continuous learning loop',
+    imageUrl: learnFlow,
+    languages: [
+      'Typescript',
+      'React',
+      'Nextjs',
+      'Expressjs',
+      'React Query',
+      'Tailwind CSS',
+      'Prisma',
+      'Postgresql',
+      'Nodejs',
+      'Vitest',
+    ],
+    link: `https://learn2pass.netlify.app/`,
+  },
   {
     id: 10,
     title: 'myPiggyVestClone',
@@ -34,60 +48,4 @@ export const projects = [
     languages: ['JavaScript', 'HTML', 'CSS', 'Bootstrap'],
     link: `https://mutairu-lawal.github.io/kc_Task7/`,
   },
-  // {
-  //   id: 7,
-  //   title: 'Quiz Fiesta',
-  //   description: `This website that allows users to take quizzes on various topics. The website features a user-friendly interface, allowing users to easily navigate through the quizzes and view their results.`,
-  //   imageUrl: quizFiesta,
-  //   languages: ['HTML', 'CSS', 'JavaScript', 'Bootstrap'],
-  //   link: `https://quiz-fiesta.netlify.app/`,
-  // },
-  // {
-  //   id: 6,
-  //   title: 'FoodBox',
-  //   description: `food delivery at its best! this website that allows users to browse and purchase food from various restaurants. The website features a user-friendly interface, allowing users to easily search for restaurants, view menus, and make purchases.`,
-  //   imageUrl: foodbox,
-  //   languages: ['HTML', 'CSS', 'Bootstrap'],
-  //   link: `https://foodbox-clone.netlify.app/`,
-  // },
-  // {
-  //   id: 5,
-  //   title: 'Modrino',
-  //   description: `LIFE IS EASY!!! This website allow users browse and purchase cars. The website features a user-friendly interface, allowing users to easily search for cars, view detailed information about each car, and make purchases.`,
-  //   imageUrl: modrino,
-  //   languages: ['HTML', 'CSS', 'Bootstrap', 'Swiper js'],
-  //   link: `https://modrino-clone.netlify.app/`,
-  // },
-  // {
-  //   id: 4,
-  //   title: 'dice-game',
-  //   description: `A simple dice game where two players roll a dice and the player with the highest number wins.`,
-  //   imageUrl: pigGame,
-  //   languages: ['HTML', 'CSS', 'JavaScript'],
-  //   link: `https://mutairu-lawal.github.io/kodeCampTask5/`,
-  // },
-  // {
-  //   id: 3,
-  //   title: 'BrainWave Academy',
-  //   description: `A comprehensive online learning platform designed to provide users with a wide range of courses and resources to enhance their skills and knowledge.`,
-  //   imageUrl: BrainWave,
-  //   languages: ['HTML', 'CSS', 'Bootstrap'],
-  //   link: `https://mutairu-lawal.github.io/BrainWave/`,
-  // },
-  // {
-  //   id: 2,
-  //   title: 'Rock Paper Scissors Game',
-  //   description: `A simple web-based Rock-Paper-Scissors game where the user competes against the computer. Built with HTML, CSS, and JavaScript, the game features real-time results, score tracking, and a user-friendly interface.`,
-  //   imageUrl: rockPaperScissors,
-  //   languages: ['HTML', 'CSS', 'JavaScript'],
-  //   link: `https://mutairu-lawal.github.io/rock-paper-scissors/`,
-  // },
-  // {
-  //   id: 1,
-  //   title: 'Moniepoint Login Page',
-  //   description: `A replica of a Moniepont microfinance bank login page, built from scratch to mimic the original's design and functionality. This project demonstrates HTML and CSS. Features: Identical design and layout to the original functional username and password input fields Responsive design for various devices and screen sizes Demonstrates understanding of front-end development best practices and design principles.`,
-  //   imageUrl: moniepointLoginPage,
-  //   languages: ['HTML', 'CSS'],
-  //   link: `https://mutairu-lawal.github.io/moniepoint-login-page-clone/`,
-  // },
 ];
